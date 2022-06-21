3ef9f1038534cf8e6fe8540ff57907d53cc45c10
José Freites <jlfu2004@gmail.com>
2022-06-21 09:12:37 -0300

    First Commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+/**
+ * ? ask for user name and
+ * use @method prompt() to show the variable userName content in the console
+ */
+
+var userName = prompt("Su nombre? ", "Fulano de Tal");
+console.log("Hola " + userName);
